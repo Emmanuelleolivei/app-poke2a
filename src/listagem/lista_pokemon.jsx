e529dd33e775aba40c99bd29664b535a935f9e4c
Emmanuelleolivei <emmanuelleoliveira878@gmail.com>
2024-05-06 08:28:57 -0300

    pokemon_listagem

---
+++ src/listagem/lista_pokemon.jsx
@@ -0,0 +1,14 @@
+import react,{useState, useEfect} from 'react'
+
+const listagem = () => {
+    <div>
+        <ul>
+            <li> pokemon 1</li>
+            <li> pokemon 2</li>
+            <li> pokemon 3</li>
+            <li> pokemon 4</li>
+            <li> pokemon 5</li>
+        </ul>
+    </div>
+}
+export default listagem
